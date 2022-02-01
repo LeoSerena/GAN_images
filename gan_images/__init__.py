1e9e8a9f0d555659066f8af1cd0b56ef63d96c3e
Leo Serena <leoserena@hotmail.ch>
2022-02-01 10:54:26 +0100

    added main files

---
+++ gan_images/__init__.py
@@ -0,0 +1,5 @@
+from gan_images.utils import *
+
+def gan_init_code():
+    file = __file__
+    print(f'this code is executed in the {file} file')
---
+++ gan_images/utils.py
@@ -0,0 +1,3 @@
+def utils_code():
+    file = __file__
+    print(f'this code is executed in the {file} file')
